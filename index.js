d749e016756b85454878fc1eb4803e38c1c042c8
Edson Vicente Carli Junior <gandbranco@gmail.com>
2019-08-21 12:34:39 -0300

    Atualizacao de teste 2
    Atualizacao de teste 2

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Node.JS!');
-}).listen(8080);
-console.log('Server running at http://localhost:8080/');
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
