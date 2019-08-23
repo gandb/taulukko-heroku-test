bffff18de8b0fdacf3d24f9c2799ffdd56f2c2a0
Edson Vicente Carli Junior <edsonvcjr@usp.br>
2019-08-23 15:29:56 -0300

    Io test

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 var express = require('express')
-var app = express()
+var io = require('socket.io')(http);
+var app = express();
+const usersConnected = 0 ;
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World index.js!')
+  response.send('Hello World index.js!' + usersConnected)
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  usersConnected++;
+});
